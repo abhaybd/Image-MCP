92a4dc4225134a3813810b7a4660370b39bb1b0e
Abhay D <a.deshpande012@gmail.com>
2025-08-12 19:29:56 -0700

    Update title

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from urllib.parse import urlparse
 from PIL import Image
 from io import BytesIO
 
-mcp = FastMCP("image-loader")
+mcp = FastMCP("Image-MCP")
 
 def is_url(path: str) -> bool:
     return urlparse(path).scheme in ["http", "https"]
